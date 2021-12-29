4ce018e30b0b10f51da2a9e2ded51ceb2f59d95d
azzyew <laisadnevescosta@gmail.com>
2021-12-29 02:46:18 -0300

    finishing main candy matches logic

--- src/App.js
+++ src/App.js
@@ -25,21 +25,101 @@ const App = () => {
     setCurrentCandyArrangement(randomCandyArrangement);
   }
 
+    const checkColumnOfFour = () => {
+        for (let i = 0; i < 39; i++) {
+            const columnOfFour = [i, i + boardWidth, i + boardWidth * 2, i + boardWidth * 3];
+            const assignedCandy = currentCandyArrangement[i];
+
+            if(columnOfFour.every(candy => currentCandyArrangement[candy] === assignedCandy)){
+                columnOfFour.forEach(candy => currentCandyArrangement[candy] = '');
+            }
+        }
+    }
+
+    const checkRowOfFour = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfFour = [i, i + 1, i + 2];
+            const assignedCandy = currentCandyArrangement[i];
+            const invalidCandy = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64];
+
+            if (invalidCandy.includes(i)) continue
+
+            if(rowOfFour.every(candy => currentCandyArrangement[candy] === assignedCandy)){
+                rowOfFour.forEach(candy => currentCandyArrangement[candy] = '');
+            }
+        }
+    }
+
+    const checkColumnOfThree = () => {
+        for (let i = 0; i < 47; i++) {
+            const columnOfThree = [i, i + boardWidth, i + boardWidth * 2];
+            const assignedCandy = currentCandyArrangement[i];
+
+            if(columnOfThree.every(candy => currentCandyArrangement[candy] === assignedCandy)){
+                columnOfThree.forEach(candy => currentCandyArrangement[candy] = '');
+            }
+        }
+    }
+
+    const checkRowOfThree = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfThree = [i, i + 1, i + 2];
+            const assignedCandy = currentCandyArrangement[i];
+            const invalidCandy = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64];
+
+            if (invalidCandy.includes(i)) continue
+
+            if(rowOfThree.every(candy => currentCandyArrangement[candy] === assignedCandy)){
+                rowOfThree.forEach(candy => currentCandyArrangement[candy] = '');
+            }
+        }
+    }
+  
+    const moveCandyToSquareBelow = () => {
+        for(let i = 0; i < 64 - boardWidth; i++) {
+
+            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+            const isFirstRow = firstRow.includes(i);
+
+            if(isFirstRow && currentCandyArrangement[i] === '') {
+                let newRandomNumber = Math.floor(Math.random() * candyColors.length);
+                currentCandyArrangement[i] = candyColors[newRandomNumber];
+            }
+
+            if((currentCandyArrangement[i + boardWidth]) === '') {
+                currentCandyArrangement[i + boardWidth] = currentCandyArrangement[i];
+                currentCandyArrangement[i] = '';
+            }
+        }
+    }
+
   useEffect(() => {
     createBoard();
   }, [])
-  
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+        checkColumnOfFour();
+        checkColumnOfThree();
+        checkRowOfFour();
+        checkRowOfThree();
+        moveCandyToSquareBelow();
+        setCurrentCandyArrangement([...currentCandyArrangement]);
+    }, 100);
+    return () => clearInterval(timer);
+  }, [checkColumnOfFour, checkColumnOfThree, checkRowOfFour, checkRowOfThree, moveCandyToSquareBelow, currentCandyArrangement])
 
   return (
     <div className="App">
-      <div className="board">
-        {currentCandyArrangement.map((candyColor, index) => (
-          <img
-            key={index}
-            style={{backgroundColor: candyColor}}
-          />
-        ))}
-      </div>
+        <div className="board">
+            {currentCandyArrangement.map((candyColor, index) => (
+                <img
+                key={index}
+                style={{backgroundColor: candyColor}}
+                alt={candyColor}
+                />
+            ))}
+        </div>
     </div>
   );
 }
